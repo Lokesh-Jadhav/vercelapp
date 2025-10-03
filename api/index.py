9aeb89544803a916ba49c40e106168b155dcf3c5
Lokesh Jadhav <142538506+Lokesh-Jadhav@users.noreply.github.com>
2025-10-04 00:17:03 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -79,3 +79,10 @@ async def options_latency():
             "Access-Control-Allow-Headers": "*"
         }
     )
+@app.middleware("http")
+async def add_cors_headers(request: Request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    return response
